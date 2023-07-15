c98930480f84fb1fd9d86bfb54a064f1ef733620
Tim Daley <timdaley@earthling.net>
2023-07-14 21:05:44 -0400

    Update util.py

--- discoverable_tph_280/util.py
+++ discoverable_tph_280/util.py
@@ -3,11 +3,11 @@ import logging
 import logging.config
 
 logger = None
-if os.path.isfile('logging.conf'):
-    logging_file='logging.conf'
+if os.path.isfile("logging.conf"):
+    logging_file = "logging.conf"
 else:
     path = os.path.dirname(__file__)
-    logging_file=f'{path}/logging.conf'
-module_name = __name__.split('.')[0]
+    logging_file = f"{path}/logging.conf"
+module_name = __name__.split(".")[0]
 logging.config.fileConfig(logging_file)
 logger = logging.getLogger(module_name)
