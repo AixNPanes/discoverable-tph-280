209abd599eff9db11764088c4bba2ba01fde15ee
Tim Daley <timdaley@earthling.net>
2023-08-30 15:21:13 -0400

    change guage to gauge

--- discoverable_tph_280/base.py
+++ discoverable_tph_280/base.py
@@ -5,7 +5,7 @@ from .util import logger
 from ha_mqtt_discoverable import EntityInfo, Subscriber, Settings
 
 
-class GuageInfo(EntityInfo):
+class GaugeInfo(EntityInfo):
     """Base class for other Info classes"""
 
     enabled_by_default: Optional[bool] = True
@@ -18,7 +18,7 @@ class GuageInfo(EntityInfo):
     suggested_display_precision: int = 1
 
 
-class Guage(Subscriber[GuageInfo]):
+class Gauge(Subscriber[GaugeInfo]):
     value_name: str = "undefined"
 
     def __init__(
@@ -31,7 +31,7 @@ class Guage(Subscriber[GuageInfo]):
     ):
         cls.info = info_class(name=name, device_class=device_class)
         cls.settings = Settings(mqtt=mqtt_settings, entity=cls.info)
-        super(Guage, cls).__init__(cls.settings, command_callback=callback)
+        super(Gauge, cls).__init__(cls.settings, command_callback=callback)
 
     def set_value(cls, value):
         cls.value = value
@@ -43,7 +43,7 @@ class Guage(Subscriber[GuageInfo]):
     def set_attributes(cls, name, value):
         logger.debug(f"set_attributes {name}, {value}")
         cls._entity.value = value
-        super(Guage, cls).set_attributes(
+        super(Gauge, cls).set_attributes(
             attributes={cls.value_name: cls._entity.value}
         )
         cls._send_action(state=cls.value)
